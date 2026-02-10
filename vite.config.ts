4ea8968fd9b5eead2ff452cae700c8562a56d260
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2026-02-11 00:43:39 +0530

    fixed vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwind from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: process.env.VITE_BASE_URL || '/tr_portfolio',
 })
